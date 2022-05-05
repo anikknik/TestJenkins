46396b024f3201e54407557c3ded4a05cabd54eb
anikknik <anikknik@gmail.com>
2022-05-05 19:34:59 +0300

    delete config from TestBase

--- src/test/java/ru/evo/tests/TestBase.java
+++ src/test/java/ru/evo/tests/TestBase.java
@@ -2,8 +2,10 @@ package ru.evo.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,10 +17,6 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         // for add Video to attach report
         DesiredCapabilities capabilities = new DesiredCapabilities();
